e4ae2bf513a07a90e613fa790fec19678b8783a0
Brandon <bdinarte@yahoo.com>
2018-05-14 22:38:05 -0600

    styles

--- rommiewebapp/src/app/app.component.ts
+++ rommiewebapp/src/app/app.component.ts
@@ -7,8 +7,7 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'Rommie';
 
-  displaying: string = 'info-general';
+  displaying: string = 'cronograma';
 
 }
